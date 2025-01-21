7d6c69165849fe24199e62bd7459b8dd8c451269
Adlin-F <adlin.firdaus@edu.sait.ca>
2025-01-21 10:42:00 -0700

    week 2 assignment

---
+++ app/week_2/page.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+import Info from "./student-info";
+export default function Page() {
+    return (
+        <div>
+            <h1>Week 2</h1>
+            <Info></Info>
+             </div>
+    );
+}
---
+++ app/week_2/student-info.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import Page from "./page";
+export default function Info() {
+  return (
+    <div>
+      <h1>Shopping List</h1>
+      <h1>Adlin Firdaus</h1>
+      <p><Link href="https://github.com/Adlin-F">Go to GitHub</Link></p>
+    </div>
+  );
+}
