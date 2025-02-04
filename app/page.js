2c085a012c8ad82dbecdc31eab6a670ee9a3cfbe
Adlin-F <adlin.firdaus@edu.sait.ca>
2025-02-04 10:06:45 -0700

    week-3 asssignment

--- app/page.js
+++ app/page.js
@@ -3,9 +3,8 @@ export default function Home() {
   return (
     <div>
       <h1>My cprg306 assignment</h1>
-      <p><Link href="/week_2">Go to Week2</Link></p>
-      
+      <p><Link href="/week-2">Go to Week2</Link></p>
+      <p><Link href="/week-3">Go to Week3</Link></p>
     </div>
   );
 }
-//<p><Link href="/week_3">Go to Week3</Link></p>
--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import Page from "./page";
 export default function Info() {
   return (
     <div>
