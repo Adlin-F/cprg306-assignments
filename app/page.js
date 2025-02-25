5e8110a66cfa22638db13eb1722799231e0a7a89
Adlin-F <adlin.firdaus@edu.sait.ca>
2025-02-25 11:03:18 -0700

    github

--- app/page.js
+++ app/page.js
@@ -6,6 +6,7 @@ export default function Home() {
       <p><Link href="/week-2">Go to Week2</Link></p>
       <p><Link href="/week-3">Go to Week3</Link></p>
       <p><Link href="/week-4">Go to Week4</Link></p>
+      <p><Link href="/week-5">Go to Week5</Link></p>
     </div>
   );
 }
