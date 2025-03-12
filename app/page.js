f98298e5f1e37f73e79f0f01dc22217fbfdb42f7
Adlin-F <adlin.firdaus@edu.sait.ca>
2025-03-11 23:41:08 -0600

    week 7

--- app/page.js
+++ app/page.js
@@ -8,6 +8,7 @@ export default function Home() {
       <p><Link href="/week-4">Go to Week4</Link></p>
       <p><Link href="/week-5">Go to Week5</Link></p>
       <p><Link href="/week-6">Go to Week6</Link></p>
+      <p><Link href="/week-7">Go to Week7</Link></p>
     </div>
   );
 }
